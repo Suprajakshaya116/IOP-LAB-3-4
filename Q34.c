2b2a34103705cd2322248fb473f746dd059210f7
Suprajakshaya116 <suprajakshayamummadi@gmail.com>
2024-09-15 22:18:21 +0530

    Add files via upload

---
+++ Q34.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() 
+{ 
+  // program to print fibonacci series
+   int sum=0,a,b,n,x,y;
+   printf("enter n = \n");
+   scanf("%d",&n);
+   a=0,b=1;
+   x=a,y=b;
+   printf("0,1,");//these two numbers are default in fibonacci series
+   while(sum<n)
+   {
+    //each number is equal lo sum of preceding two numbers 
+     sum=a+b;
+     a=b;
+     b=sum;
+     if(sum>n)
+     break;
+   printf("%d,",sum);
+   }
+   end:
+   return 0;
+}
